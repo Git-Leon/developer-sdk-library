24312bb8e346dee59311cb0702893122058ff11f
git-leon <xleonhunter@gmail.com>
2020-10-07 16:18:59 -0400

    Created OSX installer (untested

---
+++ mac-os/installer.sh
@@ -0,0 +1,6 @@
+mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+
+brew cask install python3
+brew install node
+brew cask install macdown
+brew cask install visual-studio-code
