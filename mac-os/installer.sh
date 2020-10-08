4cf2880acb877bffb0fcc83470294a4a3453c2de
Leon Christopher Hunter <xleonhunter@gmail.com>
2020-10-08 14:23:43 -0400

    Update installer.sh

--- mac-os/installer.sh
+++ mac-os/installer.sh
@@ -1,6 +1,8 @@
 mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 
 brew cask install python3
+brew tap adoptopenjdk/openjdk
+brew cask install adoptopenjdk8
 brew install node
 brew install git
 brew cask install macdown
