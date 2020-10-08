16540b22166e4a503b806131d7064131d35ad24d
git-leon <xleonhunter@gmail.com>
2020-10-08 08:50:11 -0400

    Added postman and eclipse to installer

--- windows-os/installer.bat
+++ windows-os/installer.bat
@@ -59,12 +59,14 @@ EXIT /B 0
 
 
 :MAIN
-call:defaultInstallApplication "installer_python-v3.7.3.exe" "https://www.python.org/ftp/python/3.7.3/python-3.7.3-amd64.exe"
-call:defaultInstallApplication "installer_node-v10.16.0-x64.msi" "https://nodejs.org/dist/v10.16.0/node-v10.16.0-x64.msi"
-call:defaultInstallApplication "installer_npp-v7.7.1.exe" "https://notepad-plus-plus.org/repository/7.x/7.7.1/npp.7.7.1.Installer.exe"
-call:defaultInstallApplication "installer_awesomium-v1.6.6.exe" "http://markdownpad.com/download/awesomium_v1.6.6_sdk_win.exe"
-call:defaultInstallApplication "installer_markdownpad2.exe" "http://markdownpad.com/download/markdownpad2-setup.exe"
-call:defaultInstallApplication "installer_vscode.exe" "https://az764295.vo.msecnd.net/stable/2213894ea0415ee8c85c5eea0d0ff81ecc191529/VSCodeUserSetup-ia32-1.36.1.exe"
+call:defaultInstallApplication "installer_python-v3.7.3.exe"        "https://www.python.org/ftp/python/3.7.3/python-3.7.3-amd64.exe"
+call:defaultInstallApplication "installer_node-v10.16.0-x64.msi"    "https://nodejs.org/dist/v10.16.0/node-v10.16.0-x64.msi"
+call:defaultInstallApplication "installer_npp-v7.7.1.exe"           "https://notepad-plus-plus.org/repository/7.x/7.7.1/npp.7.7.1.Installer.exe"
+call:defaultInstallApplication "installer_awesomium-v1.6.6.exe"     "http://markdownpad.com/download/awesomium_v1.6.6_sdk_win.exe"
+call:defaultInstallApplication "installer_markdownpad2.exe"         "http://markdownpad.com/download/markdownpad2-setup.exe"
+call:defaultInstallApplication "installer_postman.exe"              "https://dl.pstmn.io/download/latest/win64"
+call:defaultInstallApplication "installer_eclipse.exe"              "http://mirror.umd.edu/eclipse/oomph/epp/2020-09/R/eclipse-inst-jre-win64.exe"
+call:defaultInstallApplication "installer_vscode.exe"               "https://az764295.vo.msecnd.net/stable/2213894ea0415ee8c85c5eea0d0ff81ecc191529/VSCodeUserSetup-ia32-1.36.1.exe"
 call:installApplication "installer_intellij-v2020.2.1.exe"^
     "%curlPath% -o ^"%cd%\installer_intellij-v2020.2.1.exe^" ^"https://download-cf.jetbrains.com/idea/ideaIU-2020.2.1.exe^"^
         -H ^"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0^"^
