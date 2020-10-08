78717dbcde0f8eff6e2fceba3090fda154df95e8
Leon Christopher Hunter <xleonhunter@gmail.com>
2020-10-08 14:13:42 -0400

    Update installer.sh

--- mac-os/installer.sh
+++ mac-os/installer.sh
@@ -2,5 +2,13 @@ mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar 
 
 brew cask install python3
 brew install node
+brew install git
 brew cask install macdown
 brew cask install visual-studio-code
+brew cask install postman
+brew cask install eclipse-jee
+brew cask install intellij-idea-ce
+brew cask install pycharm-ce
+brew install maven
+brew install docker
+brew install kubernetes-cli
