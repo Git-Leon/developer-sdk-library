818eecde91c0b4e49f72e5bd3903c48f776b798e
Leon Christopher Hunter <xleonhunter@gmail.com>
2020-10-08 14:17:08 -0400

    Update installer.sh

--- mac-os/installer.sh
+++ mac-os/installer.sh
@@ -4,11 +4,11 @@ brew cask install python3
 brew install node
 brew install git
 brew cask install macdown
-brew cask install visual-studio-code
 brew cask install postman
-brew cask install eclipse-jee
+brew cask install visual-studio-code
 brew cask install intellij-idea-ce
 brew cask install pycharm-ce
+brew cask install eclipse-jee
 brew install maven
 brew install docker
 brew install kubernetes-cli
